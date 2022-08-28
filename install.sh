eb1476405eff62613effec2b338c9c9e11247183
IgorexaFabric <109515028+IgorexaFabric@users.noreply.github.com>
2022-08-28 12:35:51 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+alias ddos='echo " ·▄▄▄▄  ·▄▄▄▄       .▄▄ ·"; echo "██▪ ██ ██▪ ██▪     ▐█ ▀."; echo "▐█· ▐█▌▐█· ▐█▌▄█▀▄ ▄▀▀▀█▄"; echo "██. ██ ██. ██▐█▌.▐▌▐█▄▪▐█"; echo "▀▀▀▀▀• ▀▀▀▀▀• ▀█▄▀▪ ▀▀▀▀ "; echo ""; echo "Enter ip:"; ip=""; read ip; echo "For stop press Ctrl+C"; ping $ip -t -l 65000 > /dev/null'
+echo "DDoS was installed successfully!"
+echo "Start by typing the ddos ​​command."
