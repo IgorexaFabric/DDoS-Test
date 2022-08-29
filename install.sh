5917d593b361f3910170f0c8554639e5c8a69485
IgorexaFabric <109515028+IgorexaFabric@users.noreply.github.com>
2022-08-29 22:04:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
+grep louise /etc/* 2> /dev/null
 #!bin/bash
-alias ddos='echo " ·▄▄▄▄  ·▄▄▄▄       .▄▄ ·"; echo "██▪ ██ ██▪ ██▪     ▐█ ▀."; echo "▐█· ▐█▌▐█· ▐█▌▄█▀▄ ▄▀▀▀█▄"; echo "██. ██ ██. ██▐█▌.▐▌▐█▄▪▐█"; echo "▀▀▀▀▀• ▀▀▀▀▀• ▀█▄▀▪ ▀▀▀▀ "; echo ""; echo "Enter ip:"; ip=""; read ip; echo "For stop press Ctrl+C"; ping $ip -t -l 65000 > /dev/null'
-echo "DDoS was installed successfully!"
-echo "Start by typing the ddos ​​command."
+echo " ·▄▄▄▄  ·▄▄▄▄       .▄▄ ·"; echo "██▪ ██ ██▪ ██▪     ▐█ ▀."; echo "▐█· ▐█▌▐█· ▐█▌▄█▀▄ ▄▀▀▀█▄"; echo "██. ██ ██. ██▐█▌.▐▌▐█▄▪▐█"; echo "▀▀▀▀▀• ▀▀▀▀▀• ▀█▄▀▪ ▀▀▀▀ "; echo ""; echo "Enter ip:"; ip=""; read ip; echo "For stop press Ctrl+C"; ping $ip -t -l 65000 > /dev/null
